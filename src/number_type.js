f102ab55894a0ae25a6701e4351a8693c5667b3a
Jason Goldberger <jlgoldb2@asu.edu>
2017-06-16 02:41:54 -0700

    remove src/*.js

--- src/number_type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class NumberType {
-    validate(data) {
-        return typeof data === 'number';
-    }
-}
-exports.NumberType = NumberType;
-exports.default = new NumberType();
